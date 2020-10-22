8e9352ca896b47c950e8abcbb37b908c1a818bd0
czy <czy@DESKTOP-9LNOG4H>
2020-10-22 09:17:37 +0800

    1

---
+++ src/main/java/org/fms/eis/Application.java
@@ -0,0 +1,20 @@
+/**
+ * Author : chizf
+ * Date : 2020年10月22日 上午9:16:19
+ * Title : org.fms.eis.Application.java
+ *
+**/
+package org.fms.eis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication(scanBasePackages = "org.fms.eis", exclude = MongoAutoConfiguration.class)
+public class Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
